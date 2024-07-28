831d87c8ae3fb1b55cbcd599cbb3aaff0854ff69
Jayapal29 <99650407+Jayapal29@users.noreply.github.com>
2024-07-28 21:47:46 +0530

    Intial commit

---
+++ script.js
@@ -0,0 +1,59 @@
+const body = document.querySelector("body"),
+      nav = document.querySelector("nav"),
+      modeToggle = document.querySelector(".dark-light"),
+      searchToggle = document.querySelector(".searchToggle"),
+      sidebarOpen = document.querySelector(".sidebarOpen"),
+      siderbarClose = document.querySelector(".siderbarClose");
+
+      let getMode = localStorage.getItem("mode");
+          if(getMode && getMode === "dark-mode"){
+            body.classList.add("dark");
+          }
+
+// js code to toggle dark and light mode
+      modeToggle.addEventListener("click" , () =>{
+        modeToggle.classList.toggle("active");
+        body.classList.toggle("dark");
+
+        // js code to keep user selected mode even page refresh or file reopen
+        if(!body.classList.contains("dark")){
+            localStorage.setItem("mode" , "light-mode");
+        }else{
+            localStorage.setItem("mode" , "dark-mode");
+        }
+      });
+
+// js code to toggle search box
+        searchToggle.addEventListener("click" , () =>{
+        searchToggle.classList.toggle("active");
+      });
+ 
+      
+//   js code to toggle sidebar
+sidebarOpen.addEventListener("click" , () =>{
+    nav.classList.add("active");
+});
+
+body.addEventListener("click" , e =>{
+    let clickedElm = e.target;
+
+    if(!clickedElm.classList.contains("sidebarOpen") && !clickedElm.classList.contains("menu")){
+        nav.classList.remove("active");
+    }
+});
+
+
+// scroll
+window.addEventListener('scroll', function() {
+  const navbar = document.querySelector('nav .nav-bar');
+  if (window.scrollY > 50) { // Adjust the scroll value as needed
+      navbar.classList.add('scrolled');
+      document.querySelector('nav').style.backgroundColor = '#fff';
+  } else {
+      navbar.classList.remove('scrolled');
+      document.querySelector('nav').style.backgroundColor = 'unset';
+  }
+});
+
+
+
